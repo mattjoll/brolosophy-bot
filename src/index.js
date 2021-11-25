aaff54ffbf1a4a98cb483d72b1505b16c6359bdd
Matthew Joll <dev@mattjoll.nz>
2021-11-25 18:07:18 +1300

    feat(bot): Add basic bot implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, brobot!');
+import { Client, Intents } from 'discord.js';
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => console.log('BroBot is online!'));
+
+const token = process.env.DISCORD_TOKEN;
+client.login(token);
